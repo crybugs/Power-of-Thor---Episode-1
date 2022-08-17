ffaff5e54e9a3b8185c36d71609501f515c8544f
crybug123 <s11333@kcis.com.tw>
2022-08-17 20:44:00 +0800

    8/17-2

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ int main()
     int light_y; // the Y position of the light of power
     int initial_tx; // Thor's starting X position
     int initial_ty; // Thor's starting Y position
-    int movements[2] = {0};
     int x = light_x-initial_tx;
     int y = light_y-initial_ty;
     cin >> light_x >> light_y >> initial_tx >> initial_ty; cin.ignore();
@@ -27,41 +26,55 @@ int main()
     while (1) {
         int remaining_turns; // The remaining amount of turns Thor can move. Do not remove this line.
         cin >> remaining_turns; cin.ignore();
-        if (x>0&&y<0){
-            NE;
-            x-=1;
-            y+=1;
-        }
-        else if(x<0&&y>0){
-            NW;
-            x+=1;
-            y-=1;
-        }
-        else if(x<0&&y<0){
-            SE;
-            x+=1;
-            y+=1;
-        }
-        else if(x>0&&y>0){
-            SW;
-            x-=1;
-            y-=1;
-        }
-        else if(x>0&&y==0){
-            E;
-            x-=1;
-        }
-        else if(x<0&&y==0){
-            W;
-            x+=1;
-        }
-        else if(x==0&&y<0){
-            N;
-            y+=1;
-        }
-        else if(x==0&&y>0){
-            S;
-            y-=1;
+        while(initial_tx!=light_x&&initial_ty!=light_y){
+            if (x>0&&y<0){
+                printf("NE");
+                x-=1;
+                y+=1;
+                initial_tx+=1;
+                initial_ty+=1;
+            }
+            else if(x<0&&y>0){
+                printf("NW");
+                x+=1;
+                y-=1;
+                initial_tx-=1;
+                initial_ty+=1;
+            }
+            else if(x<0&&y<0){
+                printf("SE");
+                x+=1;
+                y+=1;
+                initial_tx+=1;
+                initial_ty-=1;
+            }
+            else if(x>0&&y>0){
+                printf("SW");
+                x-=1;
+                y-=1;
+                initial_tx-=1;
+                initial_ty-=1;
+            }
+            else if(x>0&&y==0){
+                printf("E");
+                x-=1;
+                initial_tx+=1;
+            }
+            else if(x<0&&y==0){
+                printf("W");
+                x+=1;
+                initial_tx-=1;
+            }
+            else if(x==0&&y<0){
+                printf("N");
+                y+=1;
+                initial_ty+=1;
+            }
+            else if(x==0&&y>0){
+                printf("S");
+                y-=1;
+                initial_ty-=1;
+            }
         }
 
 
