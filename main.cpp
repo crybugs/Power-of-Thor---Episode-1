6d47d2ec278b0fbaa49ce0407607257297cdc840
crybug123 <s11333@kcis.com.tw>
2022-08-17 21:10:57 +0800

    8/17-3

--- main.cpp
+++ main.cpp
@@ -18,60 +18,60 @@ int main()
     int light_y; // the Y position of the light of power
     int initial_tx; // Thor's starting X position
     int initial_ty; // Thor's starting Y position
-    int x = light_x-initial_tx;
-    int y = light_y-initial_ty;
     cin >> light_x >> light_y >> initial_tx >> initial_ty; cin.ignore();
 
     // game loop
     while (1) {
         int remaining_turns; // The remaining amount of turns Thor can move. Do not remove this line.
         cin >> remaining_turns; cin.ignore();
+        int x = light_x-initial_tx;
+        int y = light_y-initial_ty;
         while(initial_tx!=light_x&&initial_ty!=light_y){
             if (x>0&&y<0){
-                printf("NE");
+                printf("NE\n");
                 x-=1;
                 y+=1;
                 initial_tx+=1;
                 initial_ty+=1;
             }
             else if(x<0&&y>0){
-                printf("NW");
+                printf("NW\n");
                 x+=1;
                 y-=1;
                 initial_tx-=1;
                 initial_ty+=1;
             }
             else if(x<0&&y<0){
-                printf("SE");
+                printf("SE\n");
                 x+=1;
                 y+=1;
                 initial_tx+=1;
                 initial_ty-=1;
             }
             else if(x>0&&y>0){
-                printf("SW");
+                printf("SW\n");
                 x-=1;
                 y-=1;
                 initial_tx-=1;
                 initial_ty-=1;
             }
             else if(x>0&&y==0){
-                printf("E");
+                printf("E\n");
                 x-=1;
                 initial_tx+=1;
             }
             else if(x<0&&y==0){
-                printf("W");
+                printf("W\n");
                 x+=1;
                 initial_tx-=1;
             }
             else if(x==0&&y<0){
-                printf("N");
+                printf("N\n");
                 y+=1;
                 initial_ty+=1;
             }
             else if(x==0&&y>0){
-                printf("S");
+                printf("S\n");
                 y-=1;
                 initial_ty-=1;
             }
@@ -84,6 +84,5 @@ int main()
 
 
         // A single line providing the move to be made: N NE E SE S SW W or NW
-        cout << "SE" << endl;
     }
 }
